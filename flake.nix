a45195f28c9dae1509b921c2fa6bb438b1cc0ce5
Rohan Godha <hi@rohangodha.com>
2024-07-13 21:22:39 -0700

    got it working

---
+++ flake.nix
@@ -0,0 +1,26 @@
+# flake.nix skeleton:
+
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages."aarch64-darwin".default = let
+      pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+    in pkgs.buildEnv {
+      name = "home-packages";
+      paths = with pkgs; [
+
+        # general tools
+	ripgrep
+	eza
+	neovim
+	lazygit
+	gh
+
+        # ... add your tools here
+      ];
+    };
+  };
+}
