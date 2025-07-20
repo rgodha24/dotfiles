4e9db5227262e625ccd81e6e48366f78e6d98859
Rohan Godha <git@rohangodha.com>
2025-07-20 16:44:34 -0600

    more stuff

--- configuration.nix
+++ configuration.nix
@@ -76,19 +76,9 @@
     firefox
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # Enable Hyprland
   programs.hyprland.enable = true;
-
-  # Enable Fish shell system-wide
   programs.fish.enable = true;
+  programs.steam.enable = true;
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
@@ -98,33 +88,13 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable bluetooth
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
-
-  # Enable polkit for authentication
   security.polkit.enable = true;
-
-  # Enable dconf for GTK applications
   programs.dconf.enable = true;
 
   # XDG portal for screen sharing and file dialogs
@@ -136,7 +106,6 @@
     ];
   };
 
-  # Fonts
   fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk-sans
@@ -148,12 +117,6 @@
     nerd-fonts.fira-code
     nerd-fonts.intone-mono
   ];
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
